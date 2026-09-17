50eed4861ef046d8119dc25f293fe06b586f7ca6
User <crescent.ola09@gmail.com>
2026-09-17 10:57:07 +0100

    Debug auth header

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const protect = (req, res, next) => {
     try {
+        console.log("AUTH HEADER:", req.headers.authorization);
         const token = req.headers.authorization?.split(" ")[1];
 
         if (!token) {
