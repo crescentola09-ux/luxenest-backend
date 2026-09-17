c9fdca831ffe21782aa47e3d93b0981e769ca700
User <crescent.ola09@gmail.com>
2026-09-17 12:47:44 +0100

    Debug authentication

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,12 @@
 const jwt = require("jsonwebtoken");
 
 const protect = (req, res, next) => {
+    console.log("AUTH HEADER:", req.headers.authorization);
+
     try {
-        console.log("AUTH HEADER:", req.headers.authorization);
         const token = req.headers.authorization?.split(" ")[1];
+
+        console.log("TOKEN EXISTS:", !!token);
 
         if (!token) {
             return res.status(401).json({
@@ -15,18 +18,21 @@ const protect = (req, res, next) => {
             token,
             process.env.JWT_SECRET
         );
+ 
+        console.log("JWT VERIFIED:", decoded);
 
         req.user = decoded;
 
         next();
-    } catch (error) {
-            console.log("JWT ERROR:", error.message);
 
-            return res.status(401).json({
-        message: "Invalid or expired token"
-    });
+    } catch (error) {
+        console.log("JWT ERROR:", error.message);
+
+        return res.status(401).json({
+            message: "Invalid or expired token"
+        });
     }
-    };
+};
 
 const adminOnly = (req, res, next) => {
     if (!req.user || req.user.role !== "admin") {
